42bc0b0a2d6c1e8f145ad4afb72e7a3604d944dc
Bruno Alves <vespidhook@gmail.com>
2022-06-07 18:16:32 -0300

    rotas

--- routes/api.php
+++ routes/api.php
@@ -37,4 +37,31 @@ Route::middlewre('auth:api')->group(function(){
     Route::get('/warnings', [WarningController::class, 'getMyWarnings'])->name('warnings.getMyWarnings');
     Route::post('/warning', [WarningController::class, 'setWarning'])->name('warning.setWarning');
     Route::post('warning/file', [WarningController::class, 'addWarningFile'])->name('warning.addWarningFile');
+
+    // Boletos
+    Route::get('/billets', [BilletController::class, 'getAll'])->name('billets.getAll');
+
+    // Achados e perdido
+    Route::get('/foundandlost', [FoundAndLostController::class, 'getAll'])->name('foundandlost.getAll');
+    Route::post('/foundandlost', [FoundAndLostController::class, 'insert'])->name('foundandlost.insert');
+    Route::put('/foundandlost/{id}', [FoundAndLostController::class, 'update'])->name('foundandlost.update');
+
+    // Unidade
+    Route::get('/unit/{id}', [UnitController::class, 'getInfo'])->name('unit.getInfo');
+    Route::post('/unit/{id}/addperson', [UnitController::class, 'addPerson'])->name('unit.addPerson');
+    Route::post('/unit/{id}/addvehicle', [UnitController::class, 'addVehicle'])->name('unit.addVehicle');
+    Route::post('/unit/{id}/addpet', [UnitController::class, 'addPet'])->name('unit.addPet');
+    Route::post('/unit/{id}/removeperson', [UnitController::class, 'removePerson'])->name('unit.removePerson');
+    Route::post('/unit/{id}/removevehicle', [UnitController::class, 'removeVehicle'])->name('unit.removeVehicle');
+    Route::post('/unit/{id}/removepet', [UnitController::class, 'removePet'])->name('unit.removePet');
+
+    // Reservas
+    Route::get('/reservations', [ReservationController::class, 'getReservations'])->name('reservations.getReservations');
+    Route::post('/reservation/{id}', [ReservationController::class, 'setReservation'])->name('reservation.setReservation');
+
+    Route::get('/reservation/{id}/disableddates', [ReservationController::class, 'getDisabledDates'])->name('reservation.getDisabledDates');
+    Route::get('/reservation/{id}/times', [ReservationController::class, 'getTimes'])->name('reservation.getTimes');
+
+    Route::get('/myreservations', [ReservationController::class, 'getMyReservations'])->name('reservations.getMyReservations');
+    Route::delete('myreservation/{id}', [ReservationController::class, 'delMyReservation'])->name('reservation.delMyReservation');
 });
