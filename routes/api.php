a2778c7a949ed9faa7e9cb43d9c591b9b0223757
Bruno Alves <vespidhook@gmail.com>
2022-06-07 16:58:49 -0300

    rotas

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,25 @@ use App\Http\Controllers\WarningController;
 Route::get('/ping', function () {
     return ['pong'=>true];
 });
+
+Route::get('/401', [AuthController::class, 'unautorized'])->name('401');
+
+Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('/auth/register', [AuthController::class, 'register'])->name('auth.register');
+
+Route::middlewre('auth:api')->group(function(){
+    Route::post('/auth/validade', [AuthController::class, 'validate'])->name('auth.validate');
+    Route::post('auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
+    // Mudal de avisos
+    Route::get('/walls', [WalletController::class, 'getAll'])->name('walls.getAll');
+    Route::post('/walls/{id}/like', [WalletController::class, 'like'])->name('walls.like');
+
+    // Documentos
+    Route::get('/docs', [DocController::class, 'getAll'])->name('docs.getAll');
+
+    // Livro de ocorrencias
+    Route::get('/warnings', [WarningController::class, 'getMyWarnings'])->name('warnings.getMyWarnings');
+    Route::post('/warning', [WarningController::class, 'setWarning'])->name('warning.setWarning');
+    Route::post('warning/file', [WarningController::class, 'addWarningFile'])->name('warning.addWarningFile');
+});
