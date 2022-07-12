cd82cd4728fe59793336116bf89171a8ae7b9403
Bruno Alves <vespidhook@gmail.com>
2022-07-12 00:11:11 -0300

    unit{id}

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
+use App\Models\UnitPeople;
+use App\Models\UnitPet;
+use App\Models\UnitVehicle;
 use Illuminate\Http\Request;
 
 class UnitController extends Controller
 {
-    //
+    public function getInfo($id) {
+        $array = ['error' => ''];
+
+        $unit = Unit::find($id);
+        if($unit) {
+
+            $peoples = UnitPeople::where('id_unit', $id)->get();
+            $vehicles = UnitVehicle::where('id_unit', $id)->get();
+            $pets = UnitPet::where('id_unit', $id)->get();
+
+            $array['peoples'] = $peoples;
+            $array['vehicles'] = $vehicles;
+            $array['pets'] = $pets;
+
+        } else {
+            $array['error'] = 'Propriedade inexistente';
+            return $array;
+        }
+
+        return $array;
+    }
 }
