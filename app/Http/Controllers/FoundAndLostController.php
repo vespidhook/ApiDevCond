c7e2ee95bd66acce5fc7abed02c060a2079180b5
Bruno Alves <vespidhook@gmail.com>
2022-07-11 23:18:27 -0300

    foundandlost2

--- app/Http/Controllers/FoundAndLostController.php
+++ app/Http/Controllers/FoundAndLostController.php
@@ -11,7 +11,28 @@ class FoundAndLostController extends Controller
     public function getAll() {
         $array = ['error' => ''];
 
+        $lost = FoundAndLost::where('status', 'LOST')
+            ->orderBy('datecreated', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->get();
 
+        $recovered = FoundAndLost::where('status', 'RECOVERED')
+            ->orderBy('datecreated', 'DESC')
+            ->orderBy('id', 'DESC')
+            ->get();
+
+        foreach ($lost as $lostKey => $lostValue) {
+            $lost[$lostKey]['datecreated'] = date('d/m/y', strtotime($lostValue['datecreated']));
+            $lost[$lostKey]['photo'] = asset('storage/'.$lostValue['photo']);
+        }
+
+        foreach ($recovered as $recKey => $recValue) {
+            $recovered[$recKey]['datecreated'] = date('d/m/y', strtotime($recValue['datecreated']));
+            $recovered[$recKey]['photo'] = asset('storage/'.$recValue['photo']);
+        }
+
+        $array['lost'] = $lost;
+        $array['recovered'] = $recovered;
 
         return $array;
     }
